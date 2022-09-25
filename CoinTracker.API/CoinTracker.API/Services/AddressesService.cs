a46abde92456178ad4843f982168441840fdb553
davidomahony <daverock914@gmail.com>
2022-09-25 10:54:25 +0100

    Adding some exception handling on known error cases for address service

--- CoinTracker.API/CoinTracker.API/Services/AddressesService.cs
+++ CoinTracker.API/CoinTracker.API/Services/AddressesService.cs
@@ -1,11 +1,29 @@
 ﻿using CoinTracker.API.Clients;
 using CoinTracker.API.Services.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
+using System.Net;
+using System.Web.Http;
 
 namespace CoinTracker.API.Services
 {
     public class AddressesService : IAddressesService
     {
+        // I would like to put these known error responses in there own file
+        private readonly HttpResponseMessage InvalidAddressResponse = new HttpResponseMessage(HttpStatusCode.NotFound)
+        {
+            ReasonPhrase = "Address inputted is not a valid address"
+        };
+
+        private readonly HttpResponseMessage ExistingAddressResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
+        {
+            ReasonPhrase = "Address already exist in the wallet"
+        };
+
+        private readonly HttpResponseMessage NonExistingAddressResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
+        {
+            ReasonPhrase = "Address does not exist in wallet"
+        };
+
         private IList<string> addresses;
         private IAddressInfoClient addressInfoClient;
 
@@ -23,18 +41,14 @@ namespace CoinTracker.API.Services
 
         public async Task AddAddress(string address)
         {
-            // first validate address
-            // Does it exist in the wallet already
-            // Is it a valid address
-
             if (this.addresses.Contains(address))
             {
-                // return response which indicates already exists in wallet
+                throw new HttpResponseException(ExistingAddressResponse);
             }
 
             if (!(await ValidateAddressExists(address)))
             {
-                // return response which indicates address is not real maybe 404
+                throw new HttpResponseException(InvalidAddressResponse);
             }
 
             this.addresses.Add(address);
@@ -49,7 +63,7 @@ namespace CoinTracker.API.Services
         {
             if (!this.addresses.Contains(address))
             {
-                // return response which indicates already exists in wallet
+                throw new HttpResponseException(NonExistingAddressResponse);
             }
 
             this.addresses.Remove(address);
